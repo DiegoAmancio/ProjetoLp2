0466639b819a149b85004461e93fc09dacc5fec0
Isaac Meneses <isaac.meneses@ccc.ufcg.edu.br>
2017-08-03 17:19:10 -0300

    estrutura de decisao atualizada

--- projeto/src/pacotePrincipal/BluRay.java
+++ projeto/src/pacotePrincipal/BluRay.java
@@ -8,30 +8,36 @@ public class BluRay {
 	
 	public BluRay(int duracao, String classificacao){
 		this.duracao = duracao;
-		verificClassificacao(classificacao);
+		verificaClassificacao(classificacao);
 	}
 	
-	private void verificClassificacao(String classificacao){
+	private void verificaClassificacao(String classificacao){
 		
-		if(classificacao.toUpperCase().trim().equals("LIVRE")){
-			this.classificacao = Classificacao.LIVRE;
-		}
-		if(classificacao.toUpperCase().trim().equals("DEZ ANOS")){
-			this.classificacao = Classificacao.DEZ_ANOS;
-		}
-		if(classificacao.toUpperCase().trim().equals("DOZE ANOS")){
-			this.classificacao = Classificacao.DOZE_ANOS;
-		}
-		if(classificacao.toUpperCase().trim().equals("QUATORZE ANOS")){
-			this.classificacao = Classificacao.QUATORZE_ANOS;
-		}
-		if(classificacao.toUpperCase().trim().equals("DEZESSEIS ANOS")){
-			this.classificacao = Classificacao.DEZESSEIS_ANOS;
-		}
-		if(classificacao.toUpperCase().trim().equals("DEZOITO ANOS")){
-			this.classificacao = Classificacao.DEZOITO_ANOS;
+		switch(classificacao.toUpperCase().trim()){
+			case "LIVRE":
+				this.classificacao = Classificacao.LIVRE;
+				break;
+			case "DEZ ANOS":
+				this.classificacao = Classificacao.DEZ_ANOS;
+				break;
+			case "DOZE ANOS":
+				this.classificacao = Classificacao.DOZE_ANOS;
+				break;
+			case "QUATORZE ANOS":
+				this.classificacao = Classificacao.QUATORZE_ANOS;
+				break;
+			case "DEZESSEIS ANOS":
+				this.classificacao = Classificacao.DEZESSEIS_ANOS;
+				break;
+			case "DEZOITO ANOS":
+				this.classificacao = Classificacao.DEZOITO_ANOS;
+				break;
+			default:
+				throw new IllegalArgumentException();
+				
 		}
 		
+				
 	}
 	
 }
