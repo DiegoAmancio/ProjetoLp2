e3d038f436031bcf0f9f831b3b9fb61c3eb3cc9e
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2017-08-03 22:24:36 -0300

    melhorias na classe Main do EasyAccept,UsuarioController e adição do doc

--- projeto/src/Enums/Plataformas.java
+++ projeto/src/Enums/Plataformas.java
@@ -12,17 +12,17 @@ public enum Plataformas {
 	PC("PC"), MAC("MAC"), PS3("PS3"), PS4("PS4"), XBOX360("XBOX360"), XBOX_ONE("XBOX_ONE"), NINTENDO_3DS(
 			"NITENDO_3DS"), OUTRO("OUTRO");
 	
-	private String tipo;
+	private String plataforma;
 	/**
 	 * 
 	 * @param plataforma especificação da plataforma.
 	 */
 	private Plataformas(String plataforma) {
-		this.tipo = plataforma;
+		this.plataforma = plataforma;
 	}
 
 	public String getPlataforma() {
-		return tipo;
+		return plataforma;
 	}
 
 }
