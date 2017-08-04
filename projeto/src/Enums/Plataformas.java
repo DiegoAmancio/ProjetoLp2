a5c866633d3831a8ccac30824f98e0c659b19abd
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2017-08-03 22:37:13 -0300

    melhorias na classe Main do EasyAccept,UsuarioController e adição do doc

--- projeto/src/Enums/Plataformas.java
+++ projeto/src/Enums/Plataformas.java
@@ -1,16 +1,17 @@
-
 package Enums;
+
 /**
-* representação das plataformas para um jogo
-* 
-* 
-* Laboratório de Programação 2 - projeto
-* 
-* @author Diego Amancio - 116210716
-*/
+ * representação das plataformas para um jogo
+ * 
+ * 
+ * Laboratório de Programação 2 - projeto
+ * 
+ * @author Diego Amancio - 116210716
+ */
 
 public enum Plataformas {
-	PC("PC"), MAC("MAC"), PS3("PS3"), PS4("PS4"), XBOX360("XBOX360"), XBOX_ONE("XBOX_ONE"), NINTENDO_3DS("NITENDO_3DS"), OUTRO("OUTRO");
+	PC("PC"), MAC("MAC"), PS3("PS3"), PS4("PS4"), XBOX360("XBOX360"), XBOX_ONE("XBOX_ONE"), NINTENDO_3DS(
+			"NITENDO_3DS"), OUTRO("OUTRO");
 	private String valor;
 
 	private Plataformas(String plataforma) {
@@ -21,4 +22,4 @@ public enum Plataformas {
 		return valor;
 	}
 }
-	
+
