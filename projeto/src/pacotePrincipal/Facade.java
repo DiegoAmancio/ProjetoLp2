91f28e6b92a67a949b088375fea0e6918567c879
Isaac Meneses <isaac.meneses@ccc.ufcg.edu.br>
2017-08-30 23:39:32 -0300

    ashu

--- projeto/src/pacotePrincipal/Facade.java
+++ projeto/src/pacotePrincipal/Facade.java
@@ -1,12 +1,7 @@
 package pacotePrincipal;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
 
 import dados.GravaDados;
 import dados.RecuperarDados;
